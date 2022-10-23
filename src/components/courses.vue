5641a3ff5c7dc153f1f9d50f944a0c87028fc227
ivanvit100 <ivanvit100@gmail.com>
2022-10-23 18:10:15 +0300

    Courses

--- src/components/courses.vue
+++ src/components/courses.vue
@@ -1,8 +1,77 @@
 <template>
-	<div id="courses">Test message from courses</div>
+	<div id="courses">
+		<h2>Курсы</h2>
+		<div class="table_center_by_css" v-for="item in coursesData"> 
+			<h3 class="th_css">{{item.title}}</h3>
+			<div class="courseCont">
+				<div class="td_css_3"><img class="courseImg" :src="item.img" alt="courseImg"></div>
+				<div class="td_css" valign="top">{{item.text}}</div>
+				<div class="td_css_2"><button @click="" class="button_css">Перейти</button></div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style>
+#courses{
+	text-align: center;
+}
+.table_center_by_css{
+	border: 2px solid gray;
+	border-radius: 10px;
+	background: #ffffff;
+	border-collapse: separate;
+	border-spacing: 20px 10px;
+	text-align: center;
+	margin: 30px 0;
+}
+.courseCont{
+	display: grid; 
+	grid-template-columns: 40% 60%;
+}
+.th_css{
+	padding: 15px;
+	vertical-align: top;
+	font-size: 20px;
+}
+.td_css{
+	padding:20px;
+ 	vertical-align: top;
+	text-align: initial;
+}
+.button_css{
+	font-size: 20px;
+	background: #FF7800;
+	color: #ffffff;
+	border-color: #FF7800;
+	border-size: 1px;
+	padding: 5px;
+	position: absolute;
+	right: 25px;
+}
+.button_css:hover{
+	background-color: #ba5800;
+	border-color: #b35400;
+}
+.td_css_2{
+	padding: 20px;
+ 	margin-right:20px;
+}
+.td_css_3{
+	padding: 20px;
+}
+.courseImg{
+	max-width: 400px;
+	width: 100%;
+}
+@media(max-width: 600px){
+	.courseCont{
+		grid-template-columns: 100%;
+	}
+	.th_css, .td_css, .td_css_2, .td_css_3{
+		display: block;
+	}
+}
 </style>
 
 <script>
@@ -13,5 +82,10 @@ export default{
 		return{
 		}
 	},
+	computed:{
+		coursesData: function(){
+    		return curData
+    	}
+	}
 }
 </script>
